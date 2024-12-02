4eb8a4cda674b62d5e9cf36ec0c99e22ea728e7c
John Ziamos <john.ziamos@lmax.com>
2024-12-02 08:18:49 +0000

    day 2a

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,50 @@
 use std::{fs::read_to_string};
 
 fn main() {
-    let mut left: [i64; 1024] =  [0; 1024];
-    let mut right: [i64; 1024] =  [0; 1024];
+    let mut result = 0;
+    for line in read_to_string("./inputs/2.txt").unwrap().lines() {
+        let parts: Vec<i32> = line.split(" ")
+        .map(|e| e.parse::<i32>().expect("Parse fail"))
+        .collect();
 
-
-    let mut i = 0;
-    for line in read_to_string("./inputs/1.txt").unwrap().lines() {
-        let parts: Vec<_> = line.split("   ").collect();
-
-        let f : i64 = parts.first().expect("Didnt find first").parse().expect("Couldnt parse");
-        let l : i64 = parts.last().expect("Didnt find first").parse().expect("Couldnt parse");
-        left[i] = f;
-        right[i] = l;
-        i += 1;
+        result += is_safe(parts);
     }
-
-    let mut sum: i64 = 0;
-    for i in 0..1024 {
-        sum += left[i] * count(right, left[i]);
-    }
-
-    println!("Answer is: '{}'", sum);
+    println!("Number of safe rows: {}", result);
 }
 
-fn count(array: [i64; 1024], value: i64) ->  i64 {
-    let mut ret = 0;
-    for i in 0..1024 {
-        if array[i] == value {
-            ret += 1;
+fn is_safe(v: Vec<i32>) ->  i32 {
+
+    let fm = mode(v[0], v[1]);
+
+    for i in 0..v.len() - 1 {
+        let left: i32 = v[i];
+        let right: i32 = v[i + 1];
+        if fm != mode(left, right) {
+            println!("Unsafe: {}, {}, {}, {}, {}", v[0],v[1],v[2],v[3],v[4]);
+            return 0;
+        }
+
+        let distance = abs(left - right);
+        if distance < 1 || distance > 3 {
+            println!("Unsafe: {}, {}, {}, {}, {}", v[0],v[1],v[2],v[3],v[4]);
+            return 0;
         }
     }
-    return ret;
+    println!("Safe: {}, {}, {}, {}, {}", v[0],v[1],v[2],v[3],v[4]);
+     return 1;
+}
+
+fn mode(left: i32, right: i32) -> i32 {
+    if left > right {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+fn abs(i: i32) -> i32 {
+    if i < 0
+    { return -i} 
+    else { return i;}
 }
