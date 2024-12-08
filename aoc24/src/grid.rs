38e00c8d0f4a745121c2cbc581561ffb128e0554
John Ziamos <iziamos@gmail.com>
2024-12-08 19:42:07 +0000

    abit more grid lib

--- aoc24/src/grid.rs
+++ aoc24/src/grid.rs
@@ -5,6 +5,13 @@ pub struct Grid{
     data: Vec<Vec<char>>
 }
 
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Point {
+    row: usize,
+    col: usize,
+    val: char,
+}
+
 impl Grid {
     pub fn from_file(file: &str) -> Grid {
         let input = read_to_string(file).expect("Failed to open file");
@@ -16,10 +23,74 @@ impl Grid {
         Grid { data: grid }
     }
 
-    fn char_at_unsafe(&self, x: isize, y: isize) -> char {
-        self.data[y as usize][x as usize]
+    fn is_in_bounds(&self, row: isize, col:  isize) -> bool {
+        col > -1 &&
+        row > -1 &&
+        row < self.data.len() as isize &&
+        col < self.data[0].len() as isize
     }
 
+    fn char_at_unsafe(&self, row: isize, col: isize) -> char {
+        self.data[row as usize][col as usize]
+    }
+
+    fn char_at(&self, row: isize, col: isize) -> Option<char> {
+        if self.is_in_bounds(row, col) {
+            return Some(self.data[row as usize][col as usize])
+        }
+        None
+    }
+
+    fn width(&self) -> usize {
+        return self.data.len();
+    }
+
+    fn height(&self) -> usize {
+        if self.data.len() == 0 {
+            return 0;
+        }
+        return self.data[0].len();
+    }
+
+    pub fn iter(&self) -> GridIterator {
+        GridIterator {
+            grid: self,
+            row: 0,
+            col: -1,
+        }
+    }
+
+}
+
+pub struct GridIterator<'a> {
+    grid: &'a Grid,
+    row: isize,
+    col: isize,
+}
+
+impl<'a> Iterator for GridIterator<'a> {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+
+        self.col += 1;
+
+        if self.col as usize == self.grid.width() {
+            self.col = 0;
+            self.row += 1;
+        }
+
+        if self.row as usize == self.grid.height() {
+            return None;
+        }
+
+        let p = Point {
+            row: self.row as usize,
+            col: self.col as usize,
+            val: self.grid.char_at_unsafe(self.row, self.col) };
+
+        Some(p)
+    }
 }
 
 impl fmt::Display for Grid {
@@ -28,6 +99,13 @@ impl fmt::Display for Grid {
             // Join each row of chars into a string and print it
             writeln!(f, "{}", row.iter().collect::<String>())?;
         }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Point row: {}, col: {}, val: {}", self.row, self.col, self.val )?;
         Ok(())
     }
 }
--- aoc24/src/main.rs
+++ aoc24/src/main.rs
@@ -5,4 +5,7 @@ fn main() {
     let d = grid::Grid::from_file(&"./inputs/s.txt");
 
     println!("{d}");
+    for p in d.iter() {
+        println!("{}", p);
+    }
 }
